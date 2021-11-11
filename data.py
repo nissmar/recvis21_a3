1ff85b26a1e0c55ca55a8f8b1db08dc462642f53
nissmar <maruani.nissim@gmail.com>
2021-11-11 22:33:03 +0100

    data

--- data.py
+++ data.py
@@ -3,12 +3,16 @@ import os
 
 import torchvision.transforms as transforms
 
+
+my_mean = [0.5079, 0.5221, 0.4361]
+my_std = [0.2079, 0.2069, 0.2217]
+
 data_transforms_train = transforms.Compose(
     [
         transforms.AutoAugment(transforms.AutoAugmentPolicy.IMAGENET),
         transforms.Resize((224,224)),
         transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        transforms.Normalize(mean=my_mean, std=my_std),
 
 
     ]
@@ -18,6 +22,6 @@ data_transforms = transforms.Compose(
       
         transforms.Resize((224, 224)),
         transforms.ToTensor(),
-        transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
+        transforms.Normalize(mean=my_mean, std=my_std),
     ]
 )
